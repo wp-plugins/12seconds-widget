bcef68d183abfe2904345ff5ac845399ecac4557
chandima <chandima@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-26 08:20:10 +0000

    adding first version to the trunk
    git-svn-id: https://plugins.svn.wordpress.org/12seconds-widget/trunk@105267 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 12seconds-widget.php
@@ -0,0 +1,143 @@
+<?php
+/*
+Plugin Name: 12seconds Widget
+Plugin URI: http://www.turingtarpit.com/2009/03/12seconds-widget/
+Description: Adds a sidebar widget to display <a href="http://12seconds.tv">12seconds</a> video status updates. Also provides a shortcode to embed the widget in a post.
+Version: 0.2
+Author: Chandima Cumaranatunge
+Author URI: http://www.turingtarpit.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+//error_reporting(E_ALL);
+
+add_action( 'widgets_init', array( TwelveSecondsWidget::ID, 'register' ));
+register_activation_hook( __FILE__, array( TwelveSecondsWidget::ID, 'activate' ));
+register_deactivation_hook( __FILE__, array( TwelveSecondsWidget::ID, 'deactivate'));
+add_shortcode('12s', array( TwelveSecondsWidget::ID, 'tag' ));
+
+class TwelveSecondsWidget 
+{
+	const	ID 			= 'TwelveSecondsWidget';
+	const	NAME 		= '12seconds Widget';
+
+	function activate()
+	{
+		$defaults = array( 
+				'title' => __('12seconds Widget'), 
+				'username' => '',
+				'size' => 'skinny' );
+		if ( ! get_option( self::ID ))
+		{
+		  add_option( self::ID , $defaults);
+		} else {
+		  update_option( self::ID , $defaults);
+		}
+	}
+  
+	function deactivate()
+	{
+		delete_option( self::ID );
+	}
+	
+	function control()
+	{
+		$options = get_option( self::ID );
+		?>
+        <p><label><?php _e('Title:'); ?><input class="widefat" name="<?php echo self::ID.'_title' ; ?>" 
+        	type="text" value="<?php echo $options['title']; ?>" /></label></p>
+        <p><label><?php _e('12seconds username:'); ?><input class="widefat" name="<?php echo self::ID.'_username' ; ?>" 
+        	type="text" value="<?php echo $options['username']; ?>" />
+        </label></p>
+        <fieldset>
+        <legend><?php _e('Widget size:'); ?></legend>
+        <p><label>
+        <input class="radio" type="radio" name="<?php echo self::ID.'_size' ; ?>"  value="skinny" 
+			<?php checked( $options['size'], 'skinny' ); ?> />&nbsp;<?php _e('Skinny (175x290)'); ?></label><br/>
+        <label>
+        <input class="radio" type="radio" name="<?php echo self::ID.'_size' ; ?>"  value="fat" 
+			<?php checked( $options['size'], 'fat' ); ?>/>&nbsp;<?php _e('Fat (380x440)'); ?></label></p>
+        </fieldset>
+        <?php
+	   	if ( isset( $_POST[ self::ID.'_username' ]))
+		{
+			$options['title'] = attribute_escape($_POST[ self::ID.'_title' ]);
+			$options['username'] = attribute_escape($_POST[ self::ID.'_username' ]);
+			$options['size'] = attribute_escape($_POST[ self::ID.'_size' ]);
+			update_option( self::ID, $options );
+	  	}
+	}
+	
+	function display( $args = array() )
+	{
+		extract( $args );
+		$options = get_option( self::ID );
+	
+		$title = empty( $options['title'] ) ? '' : apply_filters('widget_title', $options['title']);
+		$username = $options['username'];
+		$size = $options['size'];
+	
+		echo $args['before_widget'];
+		
+		
+		if ( !empty( $title ) )
+		{ 
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+		
+		echo self::getWidgetCode( $username, $size  );
+		
+		echo $args['after_widget'];
+	}
+	
+	function getWidgetCode( $username, $size )
+	{
+		if ( empty( $username ) )
+		{ 
+			return __('<p><strong>ERROR:</strong> 12seconds <em>username</em> not specified.</p>');
+		} else {
+			switch ( strtolower( $size )) 
+			{
+				case 'skinny':
+					return '<iframe src="http://embed.12seconds.tv/i/widgetSmall?u='.$username.'" scrolling="no" allowtransparency="true" frameborder="0" width="175" height="290"></iframe>';
+					break;
+				case 'fat':
+					return '<iframe src="http://embed.12seconds.tv/i/widgetFull?u='.$username.'" scrolling="no" allowtransparency="true" frameborder="0" width="380" height="440"></iframe>';
+					break;
+			}
+		}
+	}
+	
+	// [12s username="some_username" size="fat"]
+	function tag( $args = array(), $content = null ) 
+	{
+		$defaults = array( 
+				'username' => '',
+				'size' => 'skinny' );
+		extract(shortcode_atts( $defaults, $args ));
+		return self::getWidgetCode( $username, $size );
+	}
+	
+	function register()
+	{
+		register_sidebar_widget(self::NAME, array(self::ID, 'display'));
+		register_widget_control(self::NAME, array(self::ID, 'control'));
+	}
+}
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== 12seconds Widget ===
+Tags: 12seconds,sidebar,widget,video,status,social networks,video blogging
+Requires at least: 2.5
+Tested up to: 2.7
+Stable tag: trunk
+
+Display 12seconds video status updates on the sidebar in your WordPress blog.
+
+== Description ==
+
+Adds a sidebar widget to display [12seconds](http://12seconds.tv) video status updates. For those of you who are familiar with Twitter; 12seconds is like Twitter, but with video instead of text updates. You can also embed the widget in WordPress posts and pages using shortcodes.
+
+**Features**
+
+* Displays the latest video status for a 12seconds user.
+* Can navigate to and view previous status updates.
+* You can choose to display a 'skinny' (175×290) or 'fat' (380×440) version of the widget.
+* You can embed the widget in a post or page using the `[12s username="some_user" size="fat/skinny"]` shortcode.
+
+For more info (options, screenshots, etc.) visit [the plugin homepage](http://www.turingtarpit.com/2009/03/12seconds-widget/ "12seconds Widget").
+
+== Installation ==
+
+1. Verify that you have PHP5, which is required for this plugin.
+1. Download the whole *12seconds-widget* folder into the */wp-content/plugins/* directory.
+1. Activate the plugin through the ‘Plugins’ menu in WordPress.
+
+**Use as a sidebar widget**
+
+After activating the plugin, go to the 'Widgets' section and drag the *12seconds-widget* into your chosen sidebar and configure.  You must enter a 12seconds *username*.
+
+**Use as an embedded widget**
+
+Using the "shortcodes" system in WordPress 2.5 and up, this plugin will allow you to embed the widget into your WordPress pages and posts. You can embed the widget in a post using `[12s username="some_user" size="fat/skinny"]` shortcode. The *username* attribute is required. The *size* attribute is optional and defaults to "skinny".
+
+ == Frequently Asked Questions ==
+
+= What is 12seconds? =
+12seconds is a place online for video status updates. It allows you to share what you're doing with your friends and family using short video clips.
+
+= How do I record status updates? =
+Using your browser, mobile phone or any other video recording device you can record and upload 12s of video to [12seconds.tv](http://12seconds.tv). Your videos will be displayed in the public stream and in the streams of your followers.
